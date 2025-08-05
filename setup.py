048c3af3eb2d1eec918437816cd801c01a0b30a4
Harry Lee <glaysia@uos.ac.kr>
2025-08-05 14:52:04 +0900

    from https://github.com/Schwalbe262/pyaedt_library

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyaedt_module",
-    version="0.0.0",
+    version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=[],
